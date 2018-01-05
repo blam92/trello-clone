a1f972fd9eed934302d0c96c27607609c5d64755
Santiago Iraola <iraola.santi@gmail.com>
2018-01-04 23:27:56 -0800

    changed Card to a classical component

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,30 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css';
 import Item from './Item';
+import { ItemTypes } from './Constants';
+import { DropTarget } from 'react-dnd';
 
-let Card = (props) => {
-  let nodeInput = '';
+const squareTarget = {
+  drop(props) {
+    
+  }
+};
 
-  let onKeyPress = (e) => {
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver()
+  };
+}
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+
+    let nodeInput = '';
+    this.getNodeInput = () => nodeInput;
+    this.getNodeInput = this.getNodeInput.bind(this);
+    this.setNodeInput = (node) => nodeInput = node;
+  }
+
+  onKeyPress = (e) => {
     if(e.key === 'Enter') {
-      props.addFunction(nodeInput.value, props.index);
-      nodeInput.value = '';
+      this.props.addFunction(this.getNodeInput().value, this.props.index);
+      this.getNodeInput().value = '';
     };
   }
 
-  let items = props.cardData.items.map((text, i) => {
-    return <Item text={text} key={i}/>;
-  });
-
-  return (
-    <div className="card card-list">
-      {props.addList ? null: <img id="close-icon" src="/close-icon.png" alt="close" onClick={() => props.deleteList(props.index)}/>}
-      <p>{props.cardData.title}</p>
-      <input className="card form-control" 
-      ref={(node) => nodeInput = node} 
-      onKeyPress={onKeyPress}/>
-      {items}
-    </div>
-  );
+  render() {
+    let items = this.props.cardData.items.map((text, i) => {
+      return <Item text={text} key={i} index={i}/>;
+    });
+    
+    return (
+      <div className="card card-list">
+        {this.props.addList ? null: <img id="close-icon" src="/close-icon.png" alt="close" onClick={() => this.props.deleteList(this.props.index)}/>}
+        <p>{this.props.cardData.title}</p>
+        <input className="card form-control" 
+        ref={(node) => this.setNodeInput(node)} 
+        onKeyPress={this.onKeyPress}/>
+        {items}
+      </div>
+    );
+  }
 }
 export default Card;
